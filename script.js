41be063b577220febf163331c91282dee8b3fade
Prxtik04 <pratikbhamble7861@gmail.com>
2025-01-10 19:27:25 +0530

    Update

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const hamb = document.querySelector(''header .nav-Bar .nav-list .hamb)
+const hamb = document.querySelector('.header .nav-bar .nav-list .hamb');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header .container');
+
+hamb.addEventListener('click', () => {
+    hamb.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
